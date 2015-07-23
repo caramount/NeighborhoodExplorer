99ca8c831aab41eee14f07ee62901c45c64e6e08
Ryan Cavanaugh <ryan.cavanaugh@me.com>
2015-07-22 21:12:32 -0400

    Initial Commit

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+	public float speed = 500f;
+	public float turn = 90f;
+	public float jump = 1000f;
+	public float maxSlope = 60f;
+	public Rigidbody rbody;
+
+	private bool grounded = true;
+
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Space) && grounded) 
+		{
+			rbody.AddRelativeForce(0f, jump, 0f);
+		}
+
+		float x = Input.GetAxis ("Horizontal"); // imagine [A] = -1, [D] = +1
+		float y = Input.GetAxis ("Vertical"); // imagine [W] = +1, [S] = -1
+		
+		rbody.AddRelativeForce (0f, 0f, y * speed * Time.deltaTime );
+		transform.Rotate ( 0f, x * turn * Time.deltaTime, 0f );
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		for (int i = 0; i < collision.contacts.Length; i++)
+		{
+			ContactPoint contact = collision.contacts[i];
+			if (Vector3.Angle(contact.normal, Vector3.up) < maxSlope)
+			{
+				grounded = true;
+			}
+		}
+	}
+	void OnCollisionExit()
+	{
+		grounded = false;
+	}
+}
---
+++ Assets/Scripts/DeathCubeTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathCubeTrigger : MonoBehaviour {
+
+	public ParticleSystem fireParticles;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider activator){
+		Destroy(activator.gameObject);
+		fireParticles.Play();
+	}
+}
